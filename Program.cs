6603591b5c96365c199a84092458eed65e9a7f19
«Vitaliy <remotealex@yandex.ru>
2022-10-03 23:45:49 +0300

    Скоринг массива

--- Program.cs
+++ Program.cs
@@ -18,5 +18,30 @@ string? D = Console.ReadLine();
 
 string [] array = new string [4] { A, B, C, D};
 
-Console.WriteLine("Новый массив: ");
+Console.WriteLine("Основной массив: ");
 Console.WriteLine(String.Join("; ", array));
+Console.WriteLine("Новый массив: ");
+
+string[] array2 = new string[array.Length];
+void SecondArrayWithIF(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        array2[count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array, array2);
+PrintArray(array2);
