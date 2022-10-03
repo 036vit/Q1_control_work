d279139f1d94f44ac31f824b5ceb773b31fbc37e
«Vitaliy <remotealex@yandex.ru>
2022-10-03 23:15:16 +0300

    Создание массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача:
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести в клавиаьуры, либо задать на старте выполнения алгоритма.
+
+Console.Clear ();
+
+Console.Write ("Введите символ или несколько:  ");
+string? A = (Console.ReadLine());
+
+Console.Write ("И ещё раз:  ");
+string? B = (Console.ReadLine());
+
+Console.Write ("Ещё раз:  ");
+string? C = (Console.ReadLine());
+
+Console.Write ("Последний раз:  ");
+string? D = Console.ReadLine();
+
+string [] array = new string [4] { A, B, C, D};
+
+Console.WriteLine("Новый массив: ");
+Console.WriteLine(String.Join("; ", array));
